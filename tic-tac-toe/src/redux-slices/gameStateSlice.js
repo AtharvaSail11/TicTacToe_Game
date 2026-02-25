3e6c870b4472e6854c16b2a339cad80644096397
atharvasail11 <atharvasail11@gmail.com>
2026-02-26 00:19:31 +0530

    Update: added the state to redux gameStateSlice

--- tic-tac-toe/src/redux-slices/gameStateSlice.js
+++ tic-tac-toe/src/redux-slices/gameStateSlice.js
@@ -1,26 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const gameStateSlice=createSlice({
-    name:'gameStateSlice',
-    initialState:{
-        name:'',
-        oppName:'',
-        gameId:null,
-        isWaiting:'',
-        currentDevice:'',
-        connecting:false,
-        oppId:null,
-        myId:null,
-        Symbol:null,
-        wsReady:false,
+const gameStateSlice = createSlice({
+    name: 'gameStateSlice',
+    initialState: {
+        name: '',
+        oppName: '',
+        gameId: null,
+        isWaiting: false,
+        connecting: false,
+        oppId: null,
+        myId: null,
+        Symbol: null,
+        restoredState: [],
+        wsReady: false,
+        gameState:'Waiting'
     },
-    reducers:{
-        startGame:(state)=>{
-            
+    reducers: {
+        startGame: (state, action) => {
+            state.name = action.payload.name;
+            state.oppName = action.payload.oppName;
+            state.gameId = action.payload.gameId;
+            state.isWaiting = action.payload.isWaiting;
+            state.connecting = action.payload.connecting;
+            state.oppId = action.payload.oppId;
+            state.myId = action.payload.myId;
+            state.Symbol = action.payload.Symbol;
+            state.gameState=action.payload.gameState;
+        },
+        reconnect: (state, action) => {
+            state.name = action.payload.name;
+            state.oppName = action.payload.oppName;
+            state.gameId = action.payload.gameId;
+            state.oppId = action.payload.oppId;
+            state.myId = action.payload.myId;
+            state.Symbol = action.payload.Symbol;
+            state.restoredState = action.payload.restoredState;
+            state.gameState=action.payload.gameState;
+        },
+        initWebSocket:(state,action)=>{
+            state.wsReady=action.payload.wsReady;
+        },
+        isPlayerWaiting:(state,action)=>{
+            state.isWaiting=action.payload.isWaiting
         }
     }
 
 });
+
+export const {startGame,reconnect,initWebSocket}=gameStateSlice.actions
 
 export default gameStateSlice.reducer;
